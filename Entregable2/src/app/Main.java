6882ce782a8909a881f358940ee10b9b1e70f634
Muguckz <fedemuguruza@gmail.com>
2021-09-26 20:45:57 -0300

    Se suben 2 consultas jpql

--- Entregable2/src/app/Main.java
+++ Entregable2/src/app/Main.java
@@ -3,6 +3,7 @@ package app;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import entidades.Carrera;
 import entidades.Carrera_Estudiante;
@@ -16,14 +17,31 @@ public class Main {
 		EntityManager em = emf.createEntityManager();
 		
 		Carrera tudai = new Carrera("TUDAI");
-		Estudiante federico = new Estudiante(942342, "Federico", "de Muguruza", "Masculino", 22, "Tandil");
-		Carrera_Estudiante ce = new Carrera_Estudiante(federico,tudai);
+		Estudiante federico = new Estudiante(942342, "Federico", "de Muguruza", "Masculino", 50, "Tandil");
+		Estudiante bruno = new Estudiante(4324234, "Bruno", "Tubino", "Masculino", 40, "Tandil");
+		Estudiante evy = new Estudiante(23423423, "Evelin", "Vega", "Femenino", 30, "Tandil");
+		Carrera_Estudiante ce = new Carrera_Estudiante(federico, tudai);
+		Carrera_Estudiante ce2 = new Carrera_Estudiante(bruno, tudai);
+		Carrera_Estudiante ce3 = new Carrera_Estudiante(evy, tudai);
 		
 		em.getTransaction().begin();	
 		em.persist(tudai);
 		em.persist(federico);
+		em.persist(bruno);
+		em.persist(evy);
 		em.persist(ce);
+		em.persist(ce2);
+		em.persist(ce3);
 		em.getTransaction().commit();
+		
+		// c) recuperar todos los estudiantes, y especificar algún criterio de ordenamiento simple.
+		Query q = em.createQuery("SELECT e FROM Estudiante e ORDER BY e.edad");
+		System.out.println(q.getResultList());
+		
+		// d) recuperar un estudiante, en base a su número de libreta universitaria.
+		q = em.createQuery("SELECT e FROM Estudiante e WHERE e.libreta = ?1").setParameter(1, 23423423);
+		System.out.println(q.getResultList());
+		
 		em.close();
 		emf.close();
 
