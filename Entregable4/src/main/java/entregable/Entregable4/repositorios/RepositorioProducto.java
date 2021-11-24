f0cca15f111ab4a68b30981318202b005fb52a53
Evy <cvega@alumnos.exa.unicen.edu.ar>
2021-11-24 13:46:07 -0300

    se agregaron dependencia sde manejo de springboot para heroku, se asignó h2 como db principal

--- Entregable4/src/main/java/entregable/Entregable4/repositorios/RepositorioProducto.java
+++
@@ -1,9 +0,0 @@
-package entregable.Entregable4.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import entregable.Entregable4.entidades.Producto;
-
-public interface RepositorioProducto extends JpaRepository<Producto, Integer> {
-	
-}
--- Entregable4/src/main/java/entregable/Entregable4/repositorios/RepositorioTicket.java
+++
@@ -1,8 +0,0 @@
-package entregable.Entregable4.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import entregable.Entregable4.entidades.Ticket;
-
-public interface RepositorioTicket extends JpaRepository<Ticket, Integer> {
-	
-}
