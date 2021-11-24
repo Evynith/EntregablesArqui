d67dc4a373da82963894fb63874f986df323cfcb
Evy <cvega@alumnos.exa.unicen.edu.ar>
2021-11-24 17:43:49 -0300

    debugg mostsouldproduct

--- Entregable5/src/main/resources/static/js/mostSoldProduct.js
+++ Entregable5/src/main/resources/static/js/mostSoldProduct.js
@@ -9,16 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
             `
             const url = "/tickets/most-sold-product"
             const response = await fetch(url);
-            const {name, amount, quantity, pricePerUnit} = await response.json();
+            const product = await response.json();
             productBox.innerHTML = '';
 
             productBox.innerHTML += `
             <div class="card text-white bg-dark mb-3">
                 <div class="card-body">
-                    <h5 class="card-title">Nombre: ${name}</h5>
-                    <h5 class="card-title">Monto total: $${amount}</h5>
-                    <h5 class="card-title">Cantidad: ${quantity}</h5>
-                    <h5 class="card-title">Precio por unidad: $${pricePerUnit}</h5>
+                    <h5 class="card-title">Nombre: ${product.name}</h5>
+                    <h5 class="card-title">Monto total: ${product.amount}</h5>
+                    <h5 class="card-title">Cantidad: ${product.quantity}</h5>
+                    <h5 class="card-title">Precio por unidad: ${product.pricePerUnit}</h5>
                 </div>
             </div>
             `   
